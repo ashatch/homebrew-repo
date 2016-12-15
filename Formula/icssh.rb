ebecd2c5a79be7a0620cec99a2a3d71b51207b9b
Andrew Hatch <ahatch@amplience.com>
2016-12-15 14:35:25 +0000

    icssh v1.0.1

--- Formula/icssh.rb
+++ Formula/icssh.rb
@@ -1,9 +1,9 @@
 class Icssh < Formula
   desc "SSH over a cluster for iTerm"
   homepage "https://github.com/ashatch/icssh"
-  url "https://github.com/ashatch/icssh/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "f95bb9d54a336da705b1f20ea84ee7cbb1255634512776192b2812bebeef66ca"
+  url "https://github.com/ashatch/icssh/archive/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "574c87ac71afcbb3d8bf751ee3e1171fcbe410c3c7130522e36f64448d9a18f4"
 
   def install
     bin.install 'icssh'
