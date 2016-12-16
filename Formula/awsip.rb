e9082febc143d40d34689bb40c29f7295e3e9f09
Andrew Hatch <ahatch@amplience.com>
2016-12-16 15:42:31 +0000

    awsip v1.0.1

--- Formula/awsip.rb
+++ Formula/awsip.rb
@@ -1,9 +1,9 @@
 class Awsip < Formula
   desc "Find ip addresses of AWS resources"
   homepage "https://github.com/ashatch/awsip"
-  url "https://github.com/ashatch/awsip/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "a6bd6e2f38af2cd79d3254501b03daa1eb1b00f6725a52914caeadf3ab1d7ab2"
+  url "https://github.com/ashatch/awsip/archive/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "e954a00a1975b80568efa8173f92682e6f90d6b560f3bb7a29cd3f52655a6063"
 
   def install
     bin.install 'awsip'
